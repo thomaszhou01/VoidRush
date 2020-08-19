819adf2a3cebef4a22c16c794f77db5a33a3cbd7
Thomas Zhou <tomahawktothedome@gmail.com>
2020-08-18 20:37:00 -0400

    Added first ship model

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = player.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = desiredPos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 50f;
+    public float acceleration = 4f;
+
+
+    private float activeForwardsSpeed, activeStrafeSpeed, activeHoverSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        activeForwardsSpeed = Mathf.Lerp(activeForwardsSpeed, Input.GetAxisRaw("Vertical") * forwardForce, acceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * sidewaysForce, acceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * sidewaysForce, acceleration * Time.deltaTime);
+
+        transform.position += transform.forward * activeForwardsSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime); 
+    }
+}
